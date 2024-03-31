3e80a0dea3bbcf9ec73808e9865255016906145f
luisalves23 <165508517+luisalves23@users.noreply.github.com>
2024-03-31 17:26:51 +0000

    adiciona a pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home() {
+    return <h1>Livia Catarina, eu tin amiu muito, você é minha bebezinha, se você tambem me ama, manda uma fotinha sorrindo pra mim hihihi!</h1>
+}
+
+export default Home;
