fbcaf9ddd65bbb67349f3e0ea10eebe4e8a51596
Luis Alves <165508517+luisalves23@users.noreply.github.com>
2024-05-25 18:49:16 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,78 @@
 import React from 'react';
 
 function Home() {
-  const presentes = [
-    'Livros',
-    'Flores',
-    'Chocolates',
-    'Jóias',
-    'Roupas',
-    // Adicione mais presentes conforme necessário
+  const categorias = [
+    {
+      nome: 'Coisas de Cozinha',
+      presentes: [
+        'Jogo de copo',
+        'Jogo de talher',
+        'Jogo de prato',
+        'Potes (tamanho diversos)',
+        'Jogo de pano de prato',
+        'Jogo de colher para cozinhar',
+        'Kit mesa posta',
+        'Batedeira',
+        'Liquidificador',
+        'Cafeteira',
+        'Microondas',
+        'Sanduicheira',
+        'Jogo de taças',
+        'Porta temperos',
+        'Jogo de panelas',
+        'Jogo de facas',
+        'Jarra',
+        'Jogo americano',
+        'Assadeiras',
+        'Xícaras/ canecas'
+      ]
+    },
+    {
+      nome: 'Lavanderia',
+      presentes: [
+        'Aspirador de pó',
+        'Rodo flat mob',
+        'Tábua de passar roupa',
+        'Ferro de passar roupa',
+        'Kit de ferramentas'
+      ]
+    },
+    {
+      nome: 'Quarto',
+      presentes: [
+        'Jogo de cama',
+        'Edredom',
+        'Travesseiro',
+        'Almofada',
+        'Kit cabides',
+        'Lençóis',
+        'Caixas organizadoras',
+        'Cortina/ blackout',
+        'Espelho'
+      ]
+    },
+    {
+      nome: 'Banheiro',
+      presentes: [
+        'Toalha de banho',
+        'Roupão',
+        'Kit de lavabo'
+      ]
+    }
+    // Adicione mais categorias conforme necessário
   ];
 
-  const listaDePresentes = presentes.map((presente, index) => (
-    <li key={index} style={{ fontSize: '1.2em', fontFamily: 'Arial, sans-serif' }}>
-      {presente}
-    </li>
+  const listaDeCategorias = categorias.map((categoria, index) => (
+    <div key={index}>
+      <h2>{categoria.nome}</h2>
+      <ul style={{ listStyleType: 'none', padding: 0 }}>
+        {categoria.presentes.map((presente, i) => (
+          <li key={i} style={{ fontSize: '1.2em', fontFamily: 'Arial, sans-serif' }}>
+            {presente}
+          </li>
+        ))}
+      </ul>
+    </div>
   ));
 
   const estiloTitulo = {
@@ -25,10 +84,8 @@ function Home() {
 
   return (
     <div>
-      <h2 style={estiloTitulo}>Sugestões de Presente:</h2>
-      <ul style={{ listStyleType: 'none', padding: 0 }}>
-        {listaDePresentes}
-      </ul>
+      <h1 style={estiloTitulo}>Sugestões de Presente por Categoria:</h1>
+      {listaDeCategorias}
     </div>
   );
 }
