c4d0258215f71f0a33cbb8953d4a9a1c39fe7ca2
Luis Alves <165508517+luisalves23@users.noreply.github.com>
2024-05-25 18:38:26 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,29 @@
+import React from 'react';
+
 function Home() {
-  return <h1>Sujestões de Presente:</h1>;
+  // Array de presentes
+  const presentes = [
+    'Livros',
+    'Flores',
+    'Chocolates',
+    'Jóias',
+    'Roupas',
+    // Adicione mais presentes conforme necessário
+  ];
+
+  // Mapeie a array de presentes para elementos de lista
+  const listaDePresentes = presentes.map((presente, index) => (
+    <li key={index}>{presente}</li>
+  ));
+
+  return (
+    <div>
+      <h1>Sugestões de Presente:</h1>
+      <ul>
+        {listaDePresentes}
+      </ul>
+    </div>
+  );
 }
 
 export default Home;
