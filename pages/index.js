5cef38376b5d8f44ec58f7727c23c71f1c31e970
Luis Alves <165508517+luisalves23@users.noreply.github.com>
2024-06-04 10:00:53 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 function Home() {
   return (
-    <img src="../imagem.jpg" alt="Descrição da imagem" style="width: 100%;">;
+    <img src="../Lívia & Luís Convite.pdf (1080 x 8000 px).png" alt="Descrição da imagem" style="width: 100%;">;
 }
 
 export default Home;
