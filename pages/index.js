c4a82bafa2866215144a6b03439e9977cb77e369
Luis Alves <165508517+luisalves23@users.noreply.github.com>
2024-06-04 10:39:06 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,17 @@
+import React from 'react';
+import imagem from './imagem.png';  // Importa a imagem usando caminho relativo
+
 function Home() {
+  // Defina estilos para que a imagem ocupe toda a tela
+  const styles = {
+    width: '100vw',
+    height: '100vh',
+    objectFit: 'cover'  // Garante que a imagem cubra a área inteira
+  };
+
   return (
-    <h1>Ola</h1>
+    <div>
+      <img src={imagem} alt="Imagem" style={styles} />
+    </div>
   );
 }
-
-export default Home;
