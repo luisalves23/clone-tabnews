54a18a6814f52ce194424f35faf9d4701a9885a9
Luis Alves <165508517+luisalves23@users.noreply.github.com>
2024-06-04 10:27:47 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,9 @@
+function Home() {
+  return (
+    <div>
+      <img src={`${process.env.PUBLIC_URL}/imagem.png`} alt="Descrição da imagem" style="width: 100%;" />
+    </div>
+  );
+}
 
+export default Home;
