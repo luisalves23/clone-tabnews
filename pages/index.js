17e7b6e49db1dfa0f3244776d0ec3aa98d075332
Luis Alves <165508517+luisalves23@users.noreply.github.com>
2024-06-04 10:08:40 -0300

    Delete pages/index.js

--- pages/index.js
+++
@@ -1,6 +0,0 @@
-function Home() {
-  return (
-   <div> <img src="../Lívia & Luís Convite.pdf (1080 x 8000 px).png" alt="Descrição da imagem" style="width: 100%;"> </div>;
-}
-
-export default Home;
