466f6a62ca6326d2f221caf43dfc12f738417c5e
Luis Alves <165508517+luisalves23@users.noreply.github.com>
2024-06-04 10:06:43 -0300

    Try fiz error

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 function Home() {
   return (
-    <img src="../Lívia & Luís Convite.pdf (1080 x 8000 px).png" alt="Descrição da imagem" style="width: 100%;">;
+   <div> <img src="../Lívia & Luís Convite.pdf (1080 x 8000 px).png" alt="Descrição da imagem" style="width: 100%;"> </div>;
 }
 
 export default Home;
