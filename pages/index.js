4d237b3be2a8574c7b8b8d553dfb972fa3956680
Luís Alves <luis.contato.alves@gmail.com>
2025-05-27 13:31:45 -0300

    Testando Deploy automatizado com Vercel

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-  return <h1>Lívia Alves meu amor, eu te amo! se você me ama da uma piscadinha!</h1>;
+  return (
+    <h1>
+      Lívia Alves meu amor, eu te amo! se você me ama da uma piscadinha!🥰
+    </h1>
+  );
 }
 
 export default Home;
